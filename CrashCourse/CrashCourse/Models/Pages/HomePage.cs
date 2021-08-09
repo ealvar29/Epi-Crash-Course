91b4f7aa68e156081d8963fa98e25333bb57f563
Ed <edalvarez2015@gmail.com>
2021-08-09 15:08:20 -0500

    finished up to part 9 of crash course

---
+++ CrashCourse/CrashCourse/Models/Pages/HomePage.cs
@@ -0,0 +1,23 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrashCourse.Models.Pages
+{
+    [ContentType(DisplayName = "HomePage", GUID = "5d3966a0-f6b4-42c3-b5e0-0a4f5b96d0d2", Description = "")]
+    public class HomePage : PageData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
